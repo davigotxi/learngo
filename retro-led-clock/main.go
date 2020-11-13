64b78d64fd16e0aae7c3898916e47d0a0be104de
davigotxi <davigotxi@gmail.com>
2020-11-13 16:16:30 +0700

    retro-clock animate clock

--- retro-led-clock/main.go
+++ retro-led-clock/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/inancgumus/screen"
 )
 
 // ░ █ ▄ ▀
@@ -91,26 +93,32 @@ var digits = [...]placeholder{zero, one, two, three, four, five, six, seven, eig
 
 func main() {
 
-	now := time.Now()
+	screen.Clear()
+	for {
+		screen.MoveTopLeft()
+		now := time.Now()
+		hour, min, sec := now.Hour(), now.Minute(), now.Second()
 
-	hour, min, sec := now.Hour(), now.Minute(), now.Second()
+		clock := [...]placeholder{
+			digits[hour/10], digits[hour%10],
+			separator,
+			digits[min/10], digits[min%10],
+			separator,
+			digits[sec/10], digits[sec%10],
+		}
 
-	fmt.Printf("hour: %d, min: %d, sec: %d\n", hour, min, sec)
-
-	clock := [...]placeholder{
-		digits[hour/10], digits[hour%10],
-		separator,
-		digits[min/10], digits[min%10],
-		separator,
-		digits[sec/10], digits[sec%10],
-	}
-
-	for line := range clock[0] {
-		for digit := range clock {
-			fmt.Print(clock[digit][line], " ")
+		for line := range clock[0] {
+			for index, digit := range clock {
+				next := clock[index][line]
+				if digit == separator && sec%2 == 0 {
+					next = "   "
+				}
+				fmt.Print(next, " ")
+			}
+			fmt.Println()
 		}
 		fmt.Println()
+		time.Sleep(time.Second)
 	}
-	fmt.Println()
 
 }
