0148ac4572a4a6fbb62e48f742b83369750d64c2
davigotxi <davigotxi@gmail.com>
2020-11-24 12:49:21 +0700

    file finder exercises

---
+++ 17-project-empty-file-finder-exercises/1-sort-to-a-file/main.go
@@ -0,0 +1,80 @@
+// Copyright © 2018 Inanc Gumus
+// Learn Go Programming Course
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// For more tutorials  : https://learngoprogramming.com
+// In-person training  : https://www.linkedin.com/in/inancgumus/
+// Follow me on twitter: https://twitter.com/inancgumus
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+)
+
+// ---------------------------------------------------------
+// EXERCISE: Sort and write items to a file
+//
+//  1. Get arguments from command-line
+//
+//  2. Sort them
+//
+//  3. Write the sorted slice to a file
+//
+//
+// EXPECTED OUTPUT
+//
+//   go run main.go
+//     Send me some items and I will sort them
+//
+//   go run main.go orange banana apple
+//
+//   cat sorted.txt
+//     apple
+//     banana
+//     orange
+//
+//
+// HINTS
+//
+//   + REMEMBER: os.Args is a []string
+//
+//   + String slices are sortable using `sort.Strings`
+//
+//   + Use ioutil.WriteFile to write to a file.
+//
+//   + But you need to convert []string to []byte to be able to
+//     write it to a file using the ioutil.WriteFile.
+//
+//   + To do that, create a new []byte and append the elements of your
+//     []string.
+//
+// ---------------------------------------------------------
+
+func main() {
+
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		fmt.Println("Send me some items and I will sort them")
+		return
+	}
+	sort.Strings(args)
+
+	var buffer []byte
+	for _, argument := range args {
+		buffer = append(buffer, argument...)
+		buffer = append(buffer, '\n')
+	}
+
+	err := ioutil.WriteFile("out.txt", buffer, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s", buffer)
+
+}
